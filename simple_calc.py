421bdba71ec3f750e2928d5ddb5fe70b8fa41c6a
Your Name <you@example.com>
2021-03-29 11:23:27 +0100

    Basic calculator example

---
+++ simple_calc.py
@@ -0,0 +1,16 @@
+class SimpleCalc:
+    # Add two numbers together
+    def add(self, num1, num2):
+        return num1 + num2
+
+    # Subtract one number from another
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    # Multiply two numbers
+    def multiply(self, num1, num2):
+        return num1 * num2
+
+    # Divide one number by another
+    def divide(self, num1, num2):
+        return num1 / num2
--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -8,3 +8,13 @@ class CalcTest(unittest.TestCase):
     calc = SimpleCalc()
 
     def test_add(self): # Naming convention - using 'test' in name of function
+        self.assertEqual(self.calc.add(2, 4), 6)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5, 3), 15)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(18, 6), 3)
